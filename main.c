2c99010b4e92b6d6aed5e9c316caeef2a5959722
Caio <caio.ariel@outloo.com>
2025-01-29 19:57:45 -0300

    atualização codigo

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void apresentarJogo() {
+void intruceGame() {
     printf("======================================\n");
     printf("       BEM-VINDO AO JOGO DA FORCA     \n");
     printf("======================================\n");
